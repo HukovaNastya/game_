73ea6ff91ec631a6238da03533e938bd10b72ca8
Настя гукова <hukovanasta@gmail.com>
2021-08-10 20:48:35 +0300

    add response style for width 768px

--- js/app.js
+++ js/app.js
@@ -7,14 +7,14 @@ $(document).ready(function(){
       {
         breakpoint: 1024,
         settings: {
-          slidesToShow:1,
+          slidesToShow: 1,
           slidesToScroll: 1,
           infinite: true,
           dots: false
         }
       },
       {
-        breakpoint: 600,
+        breakpoint: 767,
         settings: {
           slidesToShow: 1,
           slidesToScroll: 1
